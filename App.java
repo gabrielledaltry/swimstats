5b8190ba47a2a61f72dc641bb0b974434ec8a956
gabrielle <gabrielledaltry@gmail.com>
2023-06-25 19:50:31 +0000

    display times file

--- App.java
+++ App.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String line;
+        BufferedReader reader = null;
+
+        try{
+            reader = new BufferedReader(new FileReader("times.csv"));
+            line = reader.readLine();
+            while (line != null){
+                System.out.println(line);
+                line = reader.readLine();
+            }
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
