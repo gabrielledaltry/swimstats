29d454bf622ad59e48d69b2bfc83fe41cd808ae8
gabrielle <gabrielledaltry@gmail.com>
2023-06-25 22:15:01 +0000

    move ReadSwimTimes to SwimTime class

--- App.java
+++ App.java
@@ -1,48 +1,12 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.ReadOnlyFileSystemException;
 import java.util.ArrayList;
 
 public class App {
-    public static ArrayList<SwimTime> ReadSwimTimes(String filePath){
-        ArrayList<SwimTime> times = new ArrayList<>();
-        BufferedReader reader = null;
-        try{
-            reader = new BufferedReader(new FileReader(filePath));
-            String line;
-            // Read and discard the header line
-            reader.readLine();
-            while ((line = reader.readLine()) != null){
-                String[] parts = line.split(",");
-                SwimTime time = new SwimTime(); 
-                time.Event = parts[0]; 
-                time.Age = parts[3]; 
-                time.Time = parts[1]; 
-                times.add(time);
-            }
-        }
-        catch (IOException e){
-            System.out.println(e.getMessage());
-        }
-        finally{
-            if (reader != null){
-                try{
-                    reader.close();
-                }
-                catch(IOException e){
-                    System.out.println(e.getMessage());
-                }
-            }
-        }
-        return times; 
-    }
 
     public static void main(String[] args) {
         String filePath = "times.csv"; 
-        ArrayList<SwimTime> times = ReadSwimTimes(filePath);
+        ArrayList<SwimTime> times = SwimTime.ReadSwimTimes(filePath);
         for (SwimTime t: times){
                 System.out.printf("%s\t\t%s\t\t%s\n", t.Event, t.Time, t.Age);
-            }
+        }
     }
 }
--- SwimTime.java
+++ SwimTime.java
@@ -1,5 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class SwimTime {
     public String Event;
     public String Time;
     public String Age;
+
+    public static ArrayList<SwimTime> ReadSwimTimes(String filePath){
+        ArrayList<SwimTime> times = new ArrayList<>();
+        BufferedReader reader = null;
+        try{
+            reader = new BufferedReader(new FileReader(filePath));
+            String line;
+            // Read and discard the header line
+            reader.readLine();
+            while ((line = reader.readLine()) != null){
+                String[] parts = line.split(",");
+                SwimTime time = new SwimTime(); 
+                time.Event = parts[0]; 
+                time.Age = parts[3]; 
+                time.Time = parts[1]; 
+                times.add(time);
+            }
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        finally{
+            if (reader != null){
+                try{
+                    reader.close();
+                }
+                catch(IOException e){
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        return times; 
+    }
 }
