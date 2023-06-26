03d89dfdf62386718cce5240f354dc948f525fa5
gabrielle <gabrielledaltry@gmail.com>
2023-06-26 23:59:30 +0000

    remove function

--- SwimEvent.java
+++ SwimEvent.java
@@ -3,9 +3,4 @@ import java.util.ArrayList;
 public class SwimEvent {
     public String name;
     public ArrayList<SwimEventTime> times = new ArrayList<>();
-
-    public static ArrayList<SwimEvent> fromSwimTimes(ArrayList<SwimTime> times) {
-        ArrayList<SwimEvent> eventList = new ArrayList<>();
-        return eventList;
-    }
 }
