a3cbff9523a6d84edba7e5a48e1ef73938ea2119
gabrielle <gabrielledaltry@gmail.com>
2023-06-27 00:48:43 +0000

    add fastest time

--- SwimEvent.java
+++ SwimEvent.java
@@ -3,4 +3,22 @@ import java.util.ArrayList;
 public class SwimEvent {
     public String name;
     public ArrayList<SwimEventTime> times = new ArrayList<>();
+
+    public SwimEventTime fastestTime() {
+        // create temp variable
+        SwimEventTime fastest = null;
+        // compare the temp variable to the next time in the list
+        for (SwimEventTime t : times) {
+            if (fastest == null) {
+                fastest = t;
+            } else {
+                // if the event is faster (smaller) than temp, replace temp
+                if (t.timeInMilliseconds() < fastest.timeInMilliseconds()) {
+                    fastest = t;
+                }
+            }
+        }
+        // return temp to find the fastest time
+        return fastest;
+    }
 }
--- SwimEventTime.java
+++ SwimEventTime.java
@@ -1,4 +1,25 @@
 public class SwimEventTime {
     public String age;
-    public String time; 
+    public String time;
+
+    public int timeInMilliseconds() {
+        String[] parts = time.split(":");
+        int minutes = 0; 
+        if (parts.length > 1){
+            minutes = Integer.parseInt(parts[0]);
+        }
+        String[] secondsAndMilliseconds; 
+        if (parts.length == 1){
+            secondsAndMilliseconds = parts[0].split("\\.");
+        }
+        else{
+            secondsAndMilliseconds = parts[1].split("\\.");
+        }
+        int seconds = Integer.parseInt(secondsAndMilliseconds[0]);
+        int milliseconds = Integer.parseInt(secondsAndMilliseconds[1]);
+
+        // Convert the time components into a single value (milliseconds)
+        int totalTimeInMilliseconds = (minutes * 60 * 1000) + (seconds * 1000) + milliseconds;
+        return totalTimeInMilliseconds; 
+    }
 }
