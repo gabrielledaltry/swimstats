cc3fb0e37e2b905b28dc724ccb48aa61f516708a
gabrielle <gabrielledaltry@gmail.com>
2023-06-27 01:08:25 +0000

    add todo

--- App.java
+++ App.java
@@ -5,6 +5,10 @@ public class App {
     public static void main(String[] args) {
         String filePath = "times.csv"; 
         ArrayList<SwimTime> times = SwimTime.readSwimTimes(filePath);
+
+        // TODO: convert from swim times to swim events with swim event times
+        // ...
+
         for(SwimTime t: times){
             System.out.println(t.meet);
         }
