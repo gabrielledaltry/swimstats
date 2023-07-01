2b242669a879f3a8656862bc1dac191091365ac5
gabrielle <gabrielledaltry@gmail.com>
2023-07-01 12:00:47 +0000

    add basic test

---
+++ tests/SwimEventTimeTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SwimEventTimeTest {
+    @Test
+    public void testFoo(){
+        assertTrue(true);
+    }
+}
