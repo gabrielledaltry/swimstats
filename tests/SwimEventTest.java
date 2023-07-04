a349491afe3cb14d164cd861ecf2541c44da9501
gabrielle <gabrielledaltry@gmail.com>
2023-07-04 16:07:06 +0000

    add tests for multiple times

--- tests/SwimEventTest.java
+++ tests/SwimEventTest.java
@@ -33,4 +33,35 @@ public class SwimEventTest {
         assertEquals("1:00.03", fastest.time);
         assertEquals("12", fastest.age); 
     }
+
+    @Test
+    public void testFirstFastest(){
+        SwimEvent event = new SwimEvent(); 
+        event.times.add(new SwimEventTime("14", "59.98"));
+        event.times.add(new SwimEventTime("12", "1:00.05"));
+        SwimEventTime fastest = event.fastestTime(); 
+        assertNotNull(fastest); 
+        assertEquals(event.times.get(0), fastest); 
+    }
+
+    @Test
+    public void testLastFastest(){
+        SwimEvent event = new SwimEvent(); 
+        event.times.add(new SwimEventTime("14", "59.98"));
+        event.times.add(new SwimEventTime("16", "56.98"));
+        SwimEventTime fastest = event.fastestTime(); 
+        assertNotNull(fastest); 
+        assertEquals(event.times.get(1), fastest); 
+    }
+
+    @Test
+    public void testMiddleFastest(){
+        SwimEvent event = new SwimEvent(); 
+        event.times.add(new SwimEventTime("14", "59.98"));
+        event.times.add(new SwimEventTime("14", "56.00"));
+        event.times.add(new SwimEventTime("13", "58.00"));
+        SwimEventTime fastest = event.fastestTime(); 
+        assertNotNull(fastest); 
+        assertEquals(event.times.get(1), fastest); 
+    }
 }
